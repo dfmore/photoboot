7f0803e68b4ca6afe90c29d1640c41b436a0f480
Daniel Moreira <de8a88@gmail.com>
2023-07-09 03:44:40 +0100

    added outline to font

--- from_chatGPT.py
+++ from_chatGPT.py
@@ -1,36 +1,3 @@
-import cv2
-import ctypes
-import os
-from datetime import datetime
-import numpy as np
-from pathlib import Path
-
-# Set the desired video capture dimensions
-width = 1920
-height = 1080
-
-# Function to capture and save an image
-def capture_image(frame):
-    # Mirror the image horizontally
-    frame = cv2.flip(frame, 1)
-
-    # Get the current timestamp
-    timestamp = datetime.now().strftime("%Y-%m-%d %H-%M-%S")
-
-    # Generate the filename with timestamp
-    filename = f"captured_image_{timestamp}.jpg"
-
-    # Create the folder on the user's desktop
-    folder_name = f"PhotoBooth-{datetime.now().strftime('%Y-%m-%d')}"
-    folder_path = Path.home() / "Desktop" / folder_name
-    folder_path.mkdir(parents=True, exist_ok=True)
-
-    # Save the captured frame to the file without the countdown number
-    file_path = folder_path / filename
-    cv2.imwrite(str(file_path), frame)
-
-    return str(file_path)
-
 # Function to draw the countdown timer on the frame
 def draw_timer(frame, seconds):
     # Get the dimensions of the frame
@@ -54,77 +21,17 @@ def draw_timer(frame, seconds):
     shadow_img = np.zeros((height, width, 3), dtype=np.uint8)
     cv2.putText(shadow_img, text, (text_x + shadow_offset, text_y + shadow_offset), font, font_scale, (0, 0, 0), font_thickness, cv2.LINE_AA)
 
+    # Create the black outline
+    cv2.putText(frame, text, (text_x - 1, text_y), font, font_scale, (0, 0, 0), font_thickness + 2, cv2.LINE_AA)
+    cv2.putText(frame, text, (text_x + 1, text_y), font, font_scale, (0, 0, 0), font_thickness + 2, cv2.LINE_AA)
+    cv2.putText(frame, text, (text_x, text_y - 1), font, font_scale, (0, 0, 0), font_thickness + 2, cv2.LINE_AA)
+    cv2.putText(frame, text, (text_x, text_y + 1), font, font_scale, (0, 0, 0), font_thickness + 2, cv2.LINE_AA)
+
     # Create the white text
     cv2.putText(frame, text, (text_x, text_y), font, font_scale, (255, 255, 255), font_thickness, cv2.LINE_AA)
 
-    # Combine the shadow and frame images
-    frame = cv2.addWeighted(frame, 1, shadow_img, 0.7, 0)
+    # Combine the shadow and frame images with increased opacity
+    alpha = 0.9  # Increase the alpha value for a more opaque shadow
+    frame = cv2.addWeighted(frame, 1, shadow_img, alpha, 0)
 
     return frame
-
-# Function to display a dialog box and ask for user input
-def prompt_dialog_box(title, message):
-    response = ctypes.windll.user32.MessageBoxW(0, message, title, 1)
-    return response == 1  # Return True if "Yes" button is clicked
-
-# Main script
-def main():
-    # Display the webcam feed
-    cap = cv2.VideoCapture(0)
-
-    # Set the video capture dimensions
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
-
-    capturing = False  # Flag to indicate whether to capture image or not
-    countdown = 3  # Countdown duration
-
-    while True:
-        ret, frame = cap.read()
-
-        # Mirror the image horizontally
-        frame = cv2.flip(frame, 1)
-
-        if capturing:
-            if countdown > 0:
-                # Draw the countdown timer
-                frame = draw_timer(frame, countdown)
-                cv2.imshow("Webcam", frame)
-                cv2.waitKey(1000)
-                countdown -= 1
-            else:
-                # Capture the image after countdown reaches 0
-                image_path = capture_image(frame)
-                cv2.imshow("Captured Image", frame)
-
-                # Prompt user if they want to print the captured image
-                print_prompt = prompt_dialog_box("Print Confirmation", "Do you want to print the captured image?")
-                if print_prompt:
-                    try:
-                        os.startfile(image_path, "print")
-                        print("Printing the captured image:", image_path)
-                    except Exception as e:
-                        print("Failed to print the image:", str(e))
-                else:
-                    print("Image capture cancelled.")
-
-                # Go back to displaying the webcam feed
-                cv2.destroyWindow("Captured Image")
-                capturing = False
-                countdown = 3
-        else:
-            cv2.imshow("Webcam", frame)
-
-        key = cv2.waitKey(1)
-
-        if key == 27:  # Check for Escape key press
-            break
-
-        if key == ord("c"):  # Check for "c" key press to initiate countdown
-            capturing = True
-
-    cap.release()
-    cv2.destroyAllWindows()
-
-if __name__ == "__main__":
-    main()
--- photobooth.py
+++ photobooth.py
@@ -38,9 +38,9 @@ def draw_timer(frame, seconds):
 
     # Set the font properties
     font = cv2.FONT_HERSHEY_SIMPLEX
-    font_scale = 4
-    font_thickness = 6
-    shadow_offset = 4
+    font_scale = 10
+    font_thickness = 20
+    shadow_offset = 5
 
     # Calculate the text size
     text = str(seconds)
@@ -54,11 +54,18 @@ def draw_timer(frame, seconds):
     shadow_img = np.zeros((height, width, 3), dtype=np.uint8)
     cv2.putText(shadow_img, text, (text_x + shadow_offset, text_y + shadow_offset), font, font_scale, (0, 0, 0), font_thickness, cv2.LINE_AA)
 
+    # Create the black outline
+    cv2.putText(frame, text, (text_x - 3, text_y), font, font_scale, (0, 0, 0), font_thickness + 6, cv2.LINE_AA)
+    cv2.putText(frame, text, (text_x + 3, text_y), font, font_scale, (0, 0, 0), font_thickness + 6, cv2.LINE_AA)
+    cv2.putText(frame, text, (text_x, text_y - 3), font, font_scale, (0, 0, 0), font_thickness + 6, cv2.LINE_AA)
+    cv2.putText(frame, text, (text_x, text_y + 3), font, font_scale, (0, 0, 0), font_thickness + 6, cv2.LINE_AA)
+
     # Create the white text
     cv2.putText(frame, text, (text_x, text_y), font, font_scale, (255, 255, 255), font_thickness, cv2.LINE_AA)
 
-    # Combine the shadow and frame images
-    frame = cv2.addWeighted(frame, 1, shadow_img, 0.7, 0)
+    # Combine the shadow and frame images with increased opacity
+    alpha = 0.9  # Increase the alpha value for a more opaque shadow
+    frame = cv2.addWeighted(frame, 1, shadow_img, alpha, 0)
 
     return frame
 
