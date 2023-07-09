fc63f0469c2deed5bad2c3598775e24720dab5d4
Daniel Moreira <de8a88@gmail.com>
2023-07-09 03:18:11 +0100

    user now gets dialog asking to print

--- photobooth.py
+++ photobooth.py
@@ -1,7 +1,6 @@
 import cv2
-import win32print
+import ctypes
 from datetime import datetime
-import time
 import numpy as np
 
 # Set the desired video capture dimensions
@@ -12,38 +11,17 @@ height = 1080
 def capture_image(frame):
     # Mirror the image horizontally
     frame = cv2.flip(frame, 1)
-    
+
     # Get the current timestamp
     timestamp = datetime.now().strftime("%Y-%m-%d %H-%M-%S")
-    
+
     # Generate the filename with timestamp
     filename = f"captured_image_{timestamp}.jpg"
-    
+
     # Save the captured frame to the file without the countdown number
     cv2.imwrite(filename, frame)
 
-    # Print the captured image
-    try:
-        print_image(filename)
-    except Exception as e:
-        print("Failed to print image:", str(e))
-
-# Function to print an image
-def print_image(image_path):
-    # Open the image file
-    file = open(image_path, "rb")
-    
-    # Get the default printer name
-    default_printer = win32print.GetDefaultPrinter()
-    
-    # Create a print job and send the image to the printer
-    win32print.SetDefaultPrinter(default_printer)
-    win32print.StartDocPrinter(default_printer, 1, (image_path, None, "RAW"))
-    win32print.WritePrinter(default_printer, file.read())
-    win32print.EndDocPrinter(default_printer)
-    
-    # Close the image file
-    file.close()
+    return filename
 
 # Function to draw the countdown timer on the frame
 def draw_timer(frame, seconds):
@@ -76,24 +54,29 @@ def draw_timer(frame, seconds):
 
     return frame
 
+# Function to display a dialog box and ask for user input
+def prompt_dialog_box(title, message):
+    response = ctypes.windll.user32.MessageBoxW(0, message, title, 1)
+    return response == 1  # Return True if "Yes" button is clicked
+
 # Main script
 def main():
     # Display the webcam feed
     cap = cv2.VideoCapture(0)
-    
+
     # Set the video capture dimensions
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
-    
+
     capturing = False  # Flag to indicate whether to capture image or not
     countdown = 3  # Countdown duration
-    
+
     while True:
         ret, frame = cap.read()
-        
+
         # Mirror the image horizontally
         frame = cv2.flip(frame, 1)
-        
+
         if capturing:
             if countdown > 0:
                 # Draw the countdown timer
@@ -103,20 +86,31 @@ def main():
                 countdown -= 1
             else:
                 # Capture the image after countdown reaches 0
-                capture_image(frame)
+                image_path = capture_image(frame)
+                cv2.imshow("Captured Image", frame)
+
+                # Prompt user if they want to print the captured image
+                print_prompt = prompt_dialog_box("Print Confirmation", "Do you want to print the captured image?")
+                if print_prompt:
+                    print("Image captured successfully:", image_path)
+                else:
+                    print("Image capture cancelled.")
+
+                # Go back to displaying the webcam feed
+                cv2.destroyWindow("Captured Image")
                 capturing = False
                 countdown = 3
         else:
             cv2.imshow("Webcam", frame)
 
         key = cv2.waitKey(1)
-        
+
         if key == 27:  # Check for Escape key press
             break
-        
+
         if key == ord("c"):  # Check for "c" key press to initiate countdown
             capturing = True
-    
+
     cap.release()
     cv2.destroyAllWindows()
 
