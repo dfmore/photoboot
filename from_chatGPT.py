eac07760c3bb51184d450a0a6c14e035b914d6c2
Daniel Moreira <de8a88@gmail.com>
2023-07-15 02:26:09 +0100

    improved UI

--- from_chatGPT.py
+++ from_chatGPT.py
@@ -14,6 +14,9 @@ pygame.mixer.init()
 sound_file = 'C:/Users/Daniel Moreira/Documents/GitHub/photoboot/camera-13695.wav'
 sound = pygame.mixer.Sound(sound_file)
 
+# Set the dimensions of the captured image in the window
+window_image_size = (800, 800)
+
 class VideoStreamWidget(object):
     def __init__(self, src=0, width=1920, height=1080):
         print("Start capturing...")
@@ -215,23 +218,27 @@ def main():
     clock = pygame.time.Clock()
 
     # Set the dimensions of the Pygame window
-    window_width = 800
-    window_height = 600
+    window_width = 1300
+    window_height = 800
     window_surface = pygame.display.set_mode((window_width, window_height))
-    pygame.display.set_caption("Photobooth")
+    pygame.display.set_caption("Dan's PhotoBooth")
 
     # Create a GUI manager
     gui_manager = pygame_gui.UIManager((window_width, window_height))
 
     # Create a Pygame GUI button for capturing images
-    capture_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((100, 100), (200, 100)),
+    capture_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((50, 50), (100, 50)),
                                                   text='Capture',
                                                   manager=gui_manager)
 
     # Create a Pygame GUI button for printing the last captured image
-    print_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((100, 300), (200, 100)),
+    print_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((50, 150), (100, 50)),
                                                 text='Print',
                                                 manager=gui_manager)
+
+    # Create a Pygame GUI image for displaying the last captured image
+    image_rect = pygame.Rect((300, 0), window_image_size)
+    image_element = pygame_gui.elements.UIImage(image_rect, pygame.Surface(window_image_size), manager=gui_manager)
 
     # Display the webcam feed
     print(logo())
@@ -284,11 +291,36 @@ def main():
                 image_path = capture_image(frame)
                 cv2.imshow("Captured Image", frame)
 
+                # Load the last captured image to display in the GUI
+                last_captured_image = image_path
+                captured_image = pygame.image.load(last_captured_image)
+
+                # Calculate the dimensions of the resized image while maintaining the aspect ratio
+                image_width, image_height = captured_image.get_size()
+                max_width = window_image_size[0]
+                max_height = window_image_size[1]
+                aspect_ratio = image_width / image_height
+
+                if aspect_ratio > 1:
+                    resized_width = max_width
+                    resized_height = int(resized_width / aspect_ratio)
+                else:
+                    resized_height = max_height
+                    resized_width = int(resized_height * aspect_ratio)
+
+                # Resize the captured image to fit within the maximum dimensions while maintaining aspect ratio
+                resized_image = pygame.transform.smoothscale(captured_image, (resized_width, resized_height))
+                image_surface = pygame.Surface((max_width, max_height))
+                image_surface.fill((0, 0, 0))  # Fill with white background
+                image_rect = resized_image.get_rect(center=image_surface.get_rect().center)
+                image_surface.blit(resized_image, image_rect)
+                image_element.set_image(image_surface)
+
                 # Go back to displaying the webcam feed
                 cv2.destroyWindow("Captured Image")
                 capturing = False
                 countdown = 3
-                last_captured_image = image_path
+
         else:
             cv2.imshow("Webcam", frame)
 
