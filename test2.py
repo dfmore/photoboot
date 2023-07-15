4d06d71da6f437e3dd4780db8edf9d4a0a719462
Daniel Moreira <de8a88@gmail.com>
2023-07-15 01:39:02 +0100

    added logo to production and more test panes

---
+++ test2.py
@@ -0,0 +1,64 @@
+# Main script
+def main():
+    # Display the webcam feed
+    print(logo())
+    video_stream_widget = VideoStreamWidget(width=1900, height=1080)
+
+    capturing = False  # Flag to indicate whether to capture image or not
+    countdown = 3  # Countdown duration
+
+    while True:
+        frame = video_stream_widget.frame
+
+        # Mirror the image horizontally and rotate
+        frame = cv2.flip(frame, 1)
+
+        if capturing:
+            if countdown > 0:
+                # Draw the countdown timer
+                frame = draw_timer(frame, countdown)
+                cv2.imshow("Webcam", frame)
+                cv2.waitKey(1000)
+                countdown -= 1
+            else:
+                # Play the sound when the countdown reaches 0
+                sound.play()
+
+                # Capture the image after the countdown reaches 0
+                image_path = capture_image(frame)
+
+                # Show the captured image on the screen
+                captured_image = cv2.imread(image_path)
+                cv2.imshow("Captured Image", captured_image)
+
+                # Prompt user if they want to print the captured image
+                print_prompt = get_user_input("Do you want to print the captured image? (y/n) ")
+                if print_prompt:
+                    try:
+                        os.startfile(image_path, "print")
+                        print("Printing the captured image:", image_path)
+                    except Exception as e:
+                        print("Failed to print the image:", str(e))
+                else:
+                    print("Image capture cancelled.")
+
+                # Go back to displaying the webcam feed
+                cv2.destroyWindow("Captured Image")
+                capturing = False
+                countdown = 3
+        else:
+            cv2.imshow("Webcam", frame)
+
+        key = cv2.waitKey(1)
+
+        if key == 27:  # Check for Escape key press
+            break
+
+        if key == ord("c"):  # Check for "c" key press to initiate countdown
+            capturing = True
+
+    video_stream_widget.capture.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
